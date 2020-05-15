4767e5b544006eafe8238cc1ae7b65957b3a21df
Zheng Wei <limzhengwei_99@hotmail.com>
2020-05-15 16:47:19 +0800

    Removed debugging statements

--- src/routers/user.js
+++ src/routers/user.js
@@ -8,10 +8,10 @@ const router = new Router();
 
 router.post("/sign-up", async (req, res) => {
     try {
-        const user = new User(req.body);
+        const { body } = req;
+        const user = new User(body);
         await user.save();
         const token = await user.generateAuthToken();
-        console.log("Successfully signed up");
         res.status(201).send({ user, token });
     }
     catch (error) {
@@ -25,7 +25,6 @@ router.post("/sign-in", async (req, res) => {
         const { email, password } = req.body;
         const user = await User.signInWithEmailAndPassword(email, password);
         const token = await user.generateAuthToken();
-        console.log("Successfully signed in");
         res.send({ user, token });
     }
     catch (error) {
@@ -39,7 +38,6 @@ router.post("/sign-out", auth, async (req, res) => {
         const { user, token } = req;
         user.tokens = user.tokens.filter(each => each.token !== token);
         await user.save();
-        console.log("Successfully signed out");
         res.send();
     }
     catch (error) {
@@ -53,7 +51,6 @@ router.post("/sign-out-all", auth, async (req, res) => {
         const { user } = req;
         user.tokens = [];
         await user.save();
-        console.log("Successfully signed out from all sessions");
         res.send();
     }
     catch (error) {
@@ -80,7 +77,6 @@ router.patch("/my-account", auth, async (req, res) => {
         const { body, user } = req;
         updates.forEach(update => user[update] = body[update]);
         await user.save();
-        console.log("Successfully updated user");
         res.send(user);
     }
     catch (error) {
@@ -93,7 +89,6 @@ router.delete("/my-account", auth, async (req, res) => {
     try {
         const { user } = req;
         await user.remove();
-        console.log("Successfully deleted account");
         res.send(user);
     }
     catch (error) {
